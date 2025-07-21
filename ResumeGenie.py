c4e1c7d59b95d6b60534ad717b46837812b5758e
Sandeep <mrsandeepg0001@gmail.com>
2025-07-21 13:28:21 +0530

    Update ResumeGenie.py

--- ResumeGenie.py
+++ ResumeGenie.py
@@ -2,7 +2,7 @@ import requests
 import os
 import streamlit as st
 
-api_key = st.secrets["OPENAI_API_KEY"]
+API_KEY = st.secrets["OPENAI_API_KEY"]
 model =st.secrets.get("MODEL_NAME", "openai/gpt-3.5-turbo")
 
 
